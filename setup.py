522e771393c52cb577dda7726c627f25e9c218ac
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-28 17:04:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='lltm_cpp',
-      ext_modules=[cpp_extension.CppExtension('lltm_cpp', ['lltm.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('lltm_cpp', ['asrc/lltm.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
