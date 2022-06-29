293a052ddcea62c2a75267b861388130f57f27c7
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 09:45:59 +0800

    Update test_jit.py

--- test_jit.py
+++ test_jit.py
@@ -2,4 +2,4 @@ from torch.utils.cpp_extension import load
 
 __call__ = ["lltm_cpp"]
 
-lltm_cpp = load(name="lltm_cpp", sources=["csrc/lltm.cpp"])
+lltm_cpp = load(name="lltm_cpp", sources=["csrc/lltm.cpp"], verbose=True)
