d71fb4c3ce279f2a84db1639b56e05db90753c33
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 14:18:13 +0800

    Update lltm_cuda_kernel.cu

--- csrc/lltm_cuda_kernel.cu
+++ csrc/lltm_cuda_kernel.cu
@@ -87,9 +87,9 @@ std::vector<torch::Tensor> lltm_cuda_forward(
             input_gate.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
             output_gate.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
             candidate_cell.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>());
-  	}));
+    }));
 
-  	return {new_h, new_cell, input_gate, output_gate, candidate_cell, X, gates};
+    return {new_h, new_cell, input_gate, output_gate, candidate_cell, X, gates};
 }
 
 template <typename scalar_t>
@@ -104,11 +104,11 @@ __global__ void lltm_cuda_backward_kernel(
     const torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> candidate_cell,
     const torch::PackedTensorAccessor32<scalar_t,3,torch::RestrictPtrTraits> gate_weights
 ) {
-  	//batch index
-  	const int n = blockIdx.y;
-  	// column index
-  	const int c = blockIdx.x * blockDim.x + threadIdx.x;
-  	if (c < d_gates.size(2)){
+    //batch index
+    const int n = blockIdx.y;
+    // column index
+    const int c = blockIdx.x * blockDim.x + threadIdx.x;
+    if (c < d_gates.size(2)){
         const auto d_output_gate = tanh(new_cell[n][c]) * grad_h[n][c];
         const auto d_tanh_new_cell = output_gate[n][c] * grad_h[n][c];
         const auto d_new_cell = d_tanh(new_cell[n][c]) * d_tanh_new_cell + grad_cell[n][c];
@@ -116,7 +116,7 @@ __global__ void lltm_cuda_backward_kernel(
         d_old_cell[n][c] = d_new_cell;
         const auto d_candidate_cell = input_gate[n][c] * d_new_cell;
         const auto d_input_gate = candidate_cell[n][c] * d_new_cell;
-
+        
         d_gates[n][0][c] = d_input_gate * d_sigmoid(gate_weights[n][0][c]);
         d_gates[n][1][c] = d_output_gate * d_sigmoid(gate_weights[n][1][c]);
         d_gates[n][2][c] = d_candidate_cell * d_elu(gate_weights[n][2][c]);
@@ -134,16 +134,16 @@ std::vector<torch::Tensor> lltm_cuda_backward(
     torch::Tensor gates,
     torch::Tensor weights
 ) {
-  	auto d_old_cell = torch::zeros_like(new_cell);
-  	auto d_gates = torch::zeros_like(gates);
+    auto d_old_cell = torch::zeros_like(new_cell);
+    auto d_gates = torch::zeros_like(gates);
 
-  	const auto batch_size = new_cell.size(0);
-  	const auto state_size = new_cell.size(1);
+    const auto batch_size = new_cell.size(0);
+    const auto state_size = new_cell.size(1);
 
-  	const int threads = 1024;
-  	const dim3 blocks((state_size + threads - 1) / threads, batch_size);
+    const int threads = 1024;
+    const dim3 blocks((state_size + threads - 1) / threads, batch_size);
 
-  	AT_DISPATCH_FLOATING_TYPES(X.type().scalar_type(), "lltm_backward_cuda", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(X.type().scalar_type(), "lltm_backward_cuda", ([&] {
         lltm_cuda_backward_kernel<scalar_t><<<blocks, threads>>>(
             d_old_cell.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
             d_gates.packed_accessor32<scalar_t,3,torch::RestrictPtrTraits>(),
@@ -154,15 +154,15 @@ std::vector<torch::Tensor> lltm_cuda_backward(
             output_gate.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
             candidate_cell.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
             gates.packed_accessor32<scalar_t,3,torch::RestrictPtrTraits>());
-  	}));
+    }));
 
-  	auto d_gate_weights = d_gates.reshape({batch_size, 3*state_size});
-  	auto d_weights = d_gate_weights.t().mm(X);
-  	auto d_bias = d_gate_weights.sum(/*dim=*/0, /*keepdim=*/true);
+    auto d_gate_weights = d_gates.reshape({batch_size, 3*state_size});
+    auto d_weights = d_gate_weights.t().mm(X);
+    auto d_bias = d_gate_weights.sum(/*dim=*/0, /*keepdim=*/true);
 
-  	auto d_X = d_gate_weights.mm(weights);
-  	auto d_old_h = d_X.slice(/*dim=*/1, 0, state_size);
-  	auto d_input = d_X.slice(/*dim=*/1, state_size);
+    auto d_X = d_gate_weights.mm(weights);
+    auto d_old_h = d_X.slice(/*dim=*/1, 0, state_size);
+    auto d_input = d_X.slice(/*dim=*/1, state_size);
 
-  	return {d_old_h, d_input, d_weights, d_bias, d_old_cell, d_gates};
+    return {d_old_h, d_input, d_weights, d_bias, d_old_cell, d_gates};
 }
