58cffee52e83963bf4fa5478dca6caa35d52b2bc
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 16:05:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,9 @@ setup(
     url="https://github.com/firstelfin/torch_extension",
     ext_modules=[CppExtension('lltm.lltm_cpp', ['csrc/lltm.cpp']), CUDAExtension("lltm.lltm_cuda", ["csrc/lltm_cuda.cpp", "csrc/lltm_cuda_kernel.cu"])],
     cmdclass={'build_ext': BuildExtension},
-    packages=find_packages(),
-    py_modules=["test_cpu", "test_gpu", "test_jit", "lltm_c", "lltm_cuda", "lltm_py"],
+    package_dir={'': 'lltm'},
+    packages=find_packages(where="lltm"),
+    py_modules=["test_cpu", "test_gpu", "test_jit", "lltm_c", "lltm_cuda", "lltm_py", "__init__"],
     install_requires=["torch"],
     keywords="extensions, cpp_extensions"
 )
