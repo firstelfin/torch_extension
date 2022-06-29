08a25e5e3784eb9664225446a68da6797a71bf30
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 14:14:39 +0800

    Update lltm_cuda_kernel.cu

--- csrc/lltm_cuda_kernel.cu
+++ csrc/lltm_cuda_kernel.cu
@@ -42,17 +42,17 @@ __global__ void lltm_cuda_forward_kernel(
     torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> output_gate,
     torch::PackedTensorAccessor32<scalar_t,2,torch::RestrictPtrTraits> candidate_cell
 ) {
-  	//batch index
-  	const int n = blockIdx.y;
-  	// column index
-  	const int c = blockIdx.x * blockDim.x + threadIdx.x;
-  	if (c < gates.size(2)){
+    //batch index
+    const int n = blockIdx.y;
+    // column index
+    const int c = blockIdx.x * blockDim.x + threadIdx.x;
+    if (c < gates.size(2)){
         input_gate[n][c] = sigmoid(gates[n][0][c]);
         output_gate[n][c] = sigmoid(gates[n][1][c]);
         candidate_cell[n][c] = elu(gates[n][2][c]);
         new_cell[n][c] = old_cell[n][c] + candidate_cell[n][c] * input_gate[n][c];
         new_h[n][c] = tanh(new_cell[n][c]) * output_gate[n][c];
-  	}
+    }
 }
 
 std::vector<torch::Tensor> lltm_cuda_forward(
@@ -62,23 +62,23 @@ std::vector<torch::Tensor> lltm_cuda_forward(
     torch::Tensor old_h,
     torch::Tensor old_cell
 ) {
-  	auto X = torch::cat({old_h, input}, /*dim=*/1);
-  	auto gate_weights = torch::addmm(bias, X, weights.transpose(0, 1));
+    auto X = torch::cat({old_h, input}, /*dim=*/1);
+    auto gate_weights = torch::addmm(bias, X, weights.transpose(0, 1));
 
     const auto batch_size = old_cell.size(0);
-  	const auto state_size = old_cell.size(1);
+    const auto state_size = old_cell.size(1);
 
-  	auto gates = gate_weights.reshape({batch_size, 3, state_size});
-  	auto new_h = torch::zeros_like(old_cell);
-  	auto new_cell = torch::zeros_like(old_cell);
-  	auto input_gate = torch::zeros_like(old_cell);
-  	auto output_gate = torch::zeros_like(old_cell);
-  	auto candidate_cell = torch::zeros_like(old_cell);
+    auto gates = gate_weights.reshape({batch_size, 3, state_size});
+    auto new_h = torch::zeros_like(old_cell);
+    auto new_cell = torch::zeros_like(old_cell);
+    auto input_gate = torch::zeros_like(old_cell);
+    auto output_gate = torch::zeros_like(old_cell);
+    auto candidate_cell = torch::zeros_like(old_cell);
 
-  	const int threads = 1024;
-  	const dim3 blocks((state_size + threads - 1) / threads, batch_size);
+    const int threads = 1024;
+    const dim3 blocks((state_size + threads - 1) / threads, batch_size);
 
-  	AT_DISPATCH_FLOATING_TYPES(gates.type().scalar_type(), "lltm_forward_cuda", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(gates.type().scalar_type(), "lltm_forward_cuda", ([&] {
         lltm_cuda_forward_kernel<scalar_t><<<blocks, threads>>>(
             gates.packed_accessor32<scalar_t,3,torch::RestrictPtrTraits>(),
             old_cell.packed_accessor32<scalar_t,2,torch::RestrictPtrTraits>(),
