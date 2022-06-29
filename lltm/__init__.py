e89adc60acf1dfb9c9953ca56345288b5e34d158
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 17:24:33 +0800

    Delete __init__.py

--- lltm/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .lltm_c import LLTM as LLTM_C
-from .lltm_py import LLTM as LLTM_PY
-from .lltm_cuda import LLTM as LLTM_CUDA
-from .test import time_test
-
-
-__call__ = ["LLTM_C", "LLTM_PY", "LLTM_CUDA", "time_test"]
