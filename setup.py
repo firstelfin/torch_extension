56c74965b0f11ea655d42759692afb8b2d380a13
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 13:42:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtensio
 
 setup(
     name='lltm',
-    ext_modules=[CppExtension('lltm_cpp', ['csrc/lltm.cpp']), CUDAExtension("lltm_cuda", ["csrc/lltm_cuda.cpp", "csrc/lltm_cuda_kernel.cu"])],
+    ext_modules=[CppExtension('lltm.lltm_cpp', ['csrc/lltm.cpp']), CUDAExtension("lltm.lltm_cuda", ["csrc/lltm_cuda.cpp", "csrc/lltm_cuda_kernel.cu"])],
     cmdclass={'build_ext': BuildExtension}
 )
