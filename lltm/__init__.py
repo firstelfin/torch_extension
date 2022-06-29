b5ca29dd3207e431b8da7eab7ca6bf3b6e6b12a2
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 16:33:57 +0800

    Update __init__.py

--- lltm/__init__.py
+++ lltm/__init__.py
@@ -2,6 +2,7 @@
 from .lltm_c import LLTM as LLTM_C
 from .lltm_py import LLTM as LLTM_PY
 from .lltm_cuda import LLTM as LLTM_CUDA
+from .test import time_test
 
 
-__call__ = ["LLTM_C", "LLTM_PY", "LLTM_CUDA"]
+__call__ = ["LLTM_C", "LLTM_PY", "LLTM_CUDA", "time_test"]
