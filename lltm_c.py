8f0b76834636fb96f3792a2b9b3f6ca28d4f3b8d
firstelfin <36871890+firstelfin@users.noreply.github.com>
2022-06-29 14:37:33 +0800

    Update lltm_c.py

--- lltm_c.py
+++ lltm_c.py
@@ -18,7 +18,7 @@ class LLTMFunction(torch.autograd.Function):
     def backward(ctx, grad_h, grad_cell):
         outputs = lltm_cpp.backward(
             grad_h.contiguous(), grad_cell.contiguous(), *ctx.saved_tensors)
-        d_old_h, d_input, d_weights, d_bias, d_old_cell = outputs
+        d_old_h, d_input, d_weights, d_bias, d_old_cell, _ = outputs
         return d_input, d_weights, d_bias, d_old_h, d_old_cell
 
 
